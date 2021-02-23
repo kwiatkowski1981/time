573559b9c97f4666a33362166b51bdc42da229fb
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-23 13:32:46 +0100

    simple clock for my website

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,18 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+const clock = () => {
+    const time = new Date();
+    console.log(time.toLocaleTimeString());
+
+    const seconds = time.getSeconds();
+    const minutes = time.getMinutes();
+    const hours = time.getHours();
+
+    document.querySelector('.clock span').textContent =
+        `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+}
+setInterval(clock, 1000);
+
