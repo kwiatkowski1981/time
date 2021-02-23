b9f1ba5a5b7699ed26df5bf3d39b204cf850cbbe
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-23 19:47:37 +0100

    new project: counter. added project template.

--- src/js/index.js
+++ src/js/index.js
@@ -8,32 +8,33 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀');
 
-const d = document.querySelector('span.d');
-const h = document.querySelector('span.h');
-const m = document.querySelector('span.m');
-const s = document.querySelector('span.s');
-
-
-const endTime = new Date('2022-01-25 22:34:00').getTime();
-
-setInterval(() => {
-    const nowTime = new Date().getTime();
-    const time = endTime - nowTime;
-    console.log(time);
-    const days = Math.floor(endTime / (1000 * 60 * 60 * 24) - nowTime / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) %24);
-    const minutes = Math.floor((endTime / 1000 * 60 - nowTime / (1000 * 60)) %60);
-    const seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
-
-d.textContent = days;
-h.textContent = hours < 10 ?  '0' + hours : hours;
-m.textContent = minutes < 10 ?  '0' + minutes : minutes;
-s.textContent = seconds < 10 ?  '0' + seconds : seconds;
-
-}, 1000)
 
 
 
+
+// const d = document.querySelector('span.d');
+// const h = document.querySelector('span.h');
+// const m = document.querySelector('span.m');
+// const s = document.querySelector('span.s');
+//
+//
+// const endTime = new Date('2022-01-25 22:34:00').getTime();
+//
+// setInterval(() => {
+//     const nowTime = new Date().getTime();
+//     const time = endTime - nowTime;
+//     console.log(time);
+//     const days = Math.floor(endTime / (1000 * 60 * 60 * 24) - nowTime / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) %24);
+//     const minutes = Math.floor((endTime / 1000 * 60 - nowTime / (1000 * 60)) %60);
+//     const seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+//
+// d.textContent = days;
+// h.textContent = hours < 10 ?  '0' + hours : hours;
+// m.textContent = minutes < 10 ?  '0' + minutes : minutes;
+// s.textContent = seconds < 10 ?  '0' + seconds : seconds;
+//
+// }, 1000)
 
 
 
