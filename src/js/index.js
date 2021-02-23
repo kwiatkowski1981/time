cfc932dfcc8a432b6ed43e6f15b447350d1ab471
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-23 13:33:47 +0100

    next exercise:  timer, countdown

--- src/js/index.js
+++ src/js/index.js
@@ -8,16 +8,19 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀');
 
-const clock = () => {
-    const time = new Date();
-    console.log(time.toLocaleTimeString());
 
-    const seconds = time.getSeconds();
-    const minutes = time.getMinutes();
-    const hours = time.getHours();
 
-    document.querySelector('.clock span').textContent =
-        `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
-}
-setInterval(clock, 1000);
+
+// const clock = () => {
+//     const time = new Date();
+//     console.log(time.toLocaleTimeString());
+//
+//     const seconds = time.getSeconds();
+//     const minutes = time.getMinutes();
+//     const hours = time.getHours();
+//
+//     document.querySelector('.clock span').textContent =
+//         `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+// }
+// setInterval(clock, 1000);
 
